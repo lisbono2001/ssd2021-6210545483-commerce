8622e95bc44eaef81cf535e83ad72f85b89d78c8
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2021-04-12 08:13:24 +0700

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "products#index"
 
@@ -7,4 +11,11 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_174410) do
+ActiveRecord::Schema.define(version: 2021_04_11_200343) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2021_04_11_174410) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
     t.index ["product_id"], name: "index_reviews_on_product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "reviews", "products"
